173e01e125716790e4ac0f2db2db0f99faa38c26
Daer0n <danilatarbaya57@gmail.com>
2023-12-26 21:06:03 +0300

    feat(pages): add student and teacher main page

--- frontend/src/components/pages/studentPage/studentPage.js
+++ frontend/src/components/pages/studentPage/studentPage.js
@@ -1,11 +1,45 @@
 import React from "react";
+import "./studentPage.css";
+import Settings from "../../../assets/images/Settings.svg";
+import Vector from "../../../assets/images/Vector.svg"
 
 const StudentPage = () => {
     return (
-        <div> 
-            Hello   
+        <div className="student-main-page">
+            <nav>
+                <div className="settings">
+                    <img src={Settings} alt="settings" />
+                </div>
+            </nav>
+            <div className="main-content">
+                <div>
+                    <div className="button-description">Theme</div>
+                    <div className="theme-buttons">
+                        <button className="choice-button">Math</button>
+                        <button className="choice-button">English</button>
+                        <button className="choice-button">Nature</button>
+                    </div>
+                </div>
+                <div>
+                    <div className="button-description difficulty">Difficulty</div>
+                    <div className="difficluty-buttons">
+                        <button className="choice-button">Easy</button>
+                        <button className="choice-button">Medium</button>
+                        <button className="choice-button">Hard</button>
+                    </div>
+                </div>
+                <button className="complete-random-exercise">
+                    Complete random level
+                </button>
+            
+            </div>
+            <footer>
+            <div className="logout">
+                    <img src={Vector} alt="logout" />
+                </div>
+            </footer>
         </div>
-    )
-}
+    );
+};
 
-export default StudentPage
+export default StudentPage;
